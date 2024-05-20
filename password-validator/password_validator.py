57e8b802664d7a3cf83984fc6febc0aa29d76377
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-05-20 15:33:38 +0530

    Add validator criteria for is_valid_password() - Add placeholder for the criteria.

--- password-validator/password_validator.py
+++ password-validator/password_validator.py
@@ -5,8 +5,35 @@ def main():
     else:
         print("Invalid password.")
 
-def is_valid_password(s):
+def is_valid_password(password):
+    return(
+        check_length(password) and
+        has_uppercase(password) and
+        has_lowercase(password) and
+        has_digit(password) and
+        has_special_character(password) and
+        no_spaces(password)
+
+    )
+
+def check_length(password):
     pass
+
+def has_uppercase(password):
+    pass
+
+def has_lowercase(password):
+    pass
+
+def has_digit(password):
+    pass
+
+def has_special_character(password):
+    pass
+
+def no_spaces(password):
+    pass
+
 
 if __name__ == "__main__":
     main()
