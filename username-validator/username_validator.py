19a085790832ddcab3e0314f4107983ac9755996
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-05-20 16:47:51 +0530

    Add validation criteria for is_valid_username() function -Add placeholder for the criteria applicable

--- username-validator/username_validator.py
+++ username-validator/username_validator.py
@@ -5,7 +5,24 @@ def main():
     else:
         print("Invalid username.")
 
-def is_valid_username(password):
+def is_valid_username(username):
+    return(
+        starts_with_letter(username) and
+        check_length(username) and
+        not_ends_with_underscore(username) and
+        contain_valid_characters(username)
+    )
+
+def starts_with_letter(username):
+    pass
+
+def check_length(username):
+    pass
+
+def not_ends_with_underscore(username):
+    pass
+
+def contain_valid_characters(username):
     pass
 
 if __name__ == "__main__":
