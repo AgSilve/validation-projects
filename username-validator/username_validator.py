56c7490c08607286ab3931071219e81e34701a01
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-05-20 17:14:54 +0530

    Rewrote the whole script using RegEx module.

--- username-validator/username_validator.py
+++ username-validator/username_validator.py
@@ -1,3 +1,5 @@
+import re
+
 def main():
     username = input("Enter username: ")
     if is_valid_username(username):
@@ -6,24 +8,8 @@ def main():
         print("Invalid username.")
 
 def is_valid_username(username):
-    return(
-        starts_with_letter(username) and
-        check_length(username) and
-        not_ends_with_underscore(username) and
-        contain_valid_characters(username)
-    )
-
-def starts_with_letter(username):
-    pass
-
-def check_length(username):
-    pass
-
-def not_ends_with_underscore(username):
-    pass
-
-def contain_valid_characters(username):
-    pass
+    pattern = r"^[a-zA-Z][a-zA-Z0-9_]{1,14}$"
+    return bool(re.match(pattern, username) and not username.endswith("_"))
 
 if __name__ == "__main__":
     main()
