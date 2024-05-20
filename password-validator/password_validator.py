1eee3266d5bb7689040b5e589aa44f5e7764e8d5
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-05-20 15:17:50 +0530

    Add initial structure for password validation script
    - Implement main() function to prompt for password input
    - Add placeholder is_valid_password(s) function for future validation logic

--- password-validator/password_validator.py
+++ password-validator/password_validator.py
@@ -1,1 +1,12 @@
-print("Work in progress...")
+def main():
+    password = input("Please enter new password: ")
+    if is_valid_password(password):
+        print("Valid password.")
+    else:
+        print("Invalid password.")
+
+def is_valid_password(s):
+    pass
+
+if __name__ == "__main__":
+    main()
