5abb13e65b37ece1764b6b95638ecee2b02b7c7f
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-06-06 21:40:11 +0530

    Create basic outline for the email validator script and its README.

---
+++ email-validator/email_validator.py
@@ -0,0 +1,14 @@
+import re
+
+def main():
+    email = input('Enter your email address: ')
+    if is_valid_email(email):
+        print('Valid Email.')
+    else:
+        ('Invalid email.')
+
+def is_valid_email(email):
+    pass
+
+if __name__ == "__main__":
+    main()
